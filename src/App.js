91be28cddcd6e9b7f9b2c49b24624d168608b4f0
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-07-18 16:31:04 +0200

    part 2 and 3 done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+const regex = new RegExp(/^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+
 class App extends React.Component {
   constructor() {
     super()
@@ -12,31 +14,58 @@ class App extends React.Component {
       isSubmitted: false
     }
   }
-  handleEmailChange = () => {
-    
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value
+    })
+    regex.test(e.target.value) && this.setState({emailIsValid: true})
+      // : this.setState({emailIsValid: false})
+    // console.log(this.state.emailIsValid)
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({password: e.target.value}, () => {
+      (e.target.value.length > 5) && this.setState ({passwordIsValid: true})
+    })
   }
   
+  handleRememberMeChange = (e) => {
+    this.setState({rememberMe: !this.state.rememberMe})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault((this.state.emailIsValid && this.state.passwordIsValid) && this.setState({isSubmitted: true}))
+  }
+
   render() {
-    return (
-      <section className="m-5">
-        <h1 className="text-center">Login</h1>
-        <form>
-          <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-          </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1"></input>
-          </div>
-          <div className="mb-3 form-check">
-            <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
-            <label className="form-check-label" for="exampleCheck1">Remember me</label>
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-      </section>
+     
+      return (  
+          <section className="m-5">
+            {!this.state.isSubmitted 
+                ? <>
+                    <h1 className="text-center">Login</h1>
+                    <form onSubmit={this.handleSubmit}>
+                      <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" onChange={this.handleEmailChange} />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="inputPassword" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="inputPassword" onChange={this.handlePasswordChange}/>
+                      </div>
+                      <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" onChange={this.handleRememberMeChange}/>
+                        <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
+                      </div>
+                      <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                  </>
+                : <h1 className="text-center">bravo l'epad !</h1>
+            } 
+            
+          </section>
     )
   }
 }
+
 export default App
